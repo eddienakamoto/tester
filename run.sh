bbc6fbb200af992d6626253b12437f908f71442f
Eddie Ciccone <eddie@nakamotomining.ai>
2024-09-24 11:58:09 -0500

    first

---
+++ run.sh
@@ -0,0 +1,25 @@
+torchrun src/train.py \
+    --stage sft \
+    --do_train \
+    --use_fast_tokenizer \
+    --model_name_or_path Qwen/Qwen2-7B-Instruct \
+    --dataset my_data \
+    --template qwen \
+    --finetuning_type lora \
+    --lora_target q_proj,v_proj\
+    --output_dir ./holy-crap \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --warmup_steps 100 \
+    --weight_decay 0.1 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --ddp_timeout 9000 \
+    --learning_rate 5e-6 \
+    --lr_scheduler_type cosine \
+    --logging_steps 1 \
+    --cutoff_len 4096 \
+    --save_steps 1000 \
+    --plot_loss \
+    --num_train_epochs 3 \
+    --bf16
